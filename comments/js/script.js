62995055ac91356346e32ab2d4d2b276ea704026
Yvon MENSAH <yvonmensah22@gmail.com>
2024-08-23 13:01:37 +0100

    Comments

---
+++ comments/js/script.js
@@ -0,0 +1,21 @@
+//Single Line Comments
+
+    // Change heading:
+    document.getElementById("myH").innerHTML = "JavaScript Comments";
+    // Change paragraph:
+    document.getElementById("myP").innerHTML = "My first paragraph.";
+
+    let x = 5;      // Declare x, give it the value of 5
+    let y = x + 2;  // Declare y, give it the value of x + 2 
+
+    // Write y to demo:
+    document.getElementById("demo").innerHTML = y;
+
+// Multi-line Comments
+
+    /*
+    The code below will change
+    the heading with id = "myH"
+    and the paragraph with id = "myP"
+    */
+    document.getElementById("demo1").innerHTML = "My second paragraph.";
